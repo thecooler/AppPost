7026ac01179e9cd7c1fe3eddd8d73e96418742cf
Joseph Forte <joeforte@Josephs-MacBook.local>
2014-09-13 19:05:29 -0400

    first rev of project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+   
+    @IBOutlet weak var enterNameTextField: UITextField!
+   
+    @IBOutlet weak var enterAMessageTextField: UITextField!
+   
+    @IBOutlet weak var mailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +29,12 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        // Code will evaluate when we press the button
+        messageLabel.hidden = false
+        
+        
+        
+    }
 }
 
